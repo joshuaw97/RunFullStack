3af382b56621049c6bafa8a8bbf0788d755b8ba3
joshuaw97 <joshuaweaver2016@gmail.com>
2024-06-12 03:48:51 -0400

    added database page and added styling to navbar

--- frontend/my-app/app/components/Navbar.js
+++ frontend/my-app/app/components/Navbar.js
@@ -8,6 +8,7 @@ export default function Navbar() {
             <h1>Run Tracker</h1>
             <Link href="/">Home</Link>
             <Link href="/weather">Weather</Link>
+            <Link href="/database">Database</Link>
         </nav>
         
 
---
+++ frontend/my-app/app/database/page.js
@@ -0,0 +1,6 @@
+
+
+
+export default function Page() {
+    return <p>Welcome to the database! Here you can add runs to the database by filling out a form. </p>;
+  }
